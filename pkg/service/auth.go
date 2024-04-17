96f9e6e4461d82f5cbc1f74f58f89ebd8f2fc24d
Siarhei Samoshyn <gds.grey@gmail.com>
2024-04-17 19:47:27 +0300

    registration

---
+++ pkg/service/auth.go
@@ -0,0 +1,61 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	todo "github.com/ssamoshin-ms/golang_test"
+	"github.com/ssamoshin-ms/golang_test/pkg/repository"
+)
+
+const (
+	salt       = "qwerty"
+	signingKey = "#qwsq1"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(12 * time.Hour).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	fmt.Println(token.SignedString([]byte(signingKey)))
+
+	return token.SignedString([]byte(signingKey))
+
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
